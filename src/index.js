27e798f5a911582b0835f4241fc0d6b33a65149c
Zsombor Makker <makker.zsombor@gmail.com>
2022-10-12 14:14:31 +0200

    Username fix done

--- src/index.js
+++ src/index.js
@@ -5,15 +5,17 @@ document.addEventListener('DOMContentLoaded', () => {
         let user = document.getElementById('felhasznalo').value;
         let email = document.getElementById('bemail').value;
         let pass = document.getElementById('jelszo').value;
+        // Username
         if (user.length <= 30 && user.length >= 6) {
             if (user.includes('..') || user.includes(',') || user.includes('+') ||
                 user.includes('-') || user.includes('_') || user.includes('<') || user.includes('>') ||
-                user.includes('&')) {
+                user.includes('&') || user.startsWith('.') || user.endsWith('.')) {
                 console.log('A felhasználónév nem tartalmazhat speciális karaktert!');
             }
         }
         else if (user.length > 30 || user.length < 6) {
-            console.log('Rossz a felhazsnálónév');
+            console.log('Rossz a felhazsnálónév hosszúság');
         }
+        // Password
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -6,15 +6,18 @@ document.addEventListener('DOMContentLoaded', () => {
         let user  = (document.getElementById('felhasznalo') as HTMLInputElement).value;
         let email = (document.getElementById('bemail') as HTMLInputElement).value;
         let pass = (document.getElementById('jelszo') as HTMLInputElement).value;
+        // Username
         if (user.length <= 30 && user.length >= 6) {
             if (user.includes('..') || user.includes(',') || user.includes('+') || 
             user.includes('-') || user.includes('_') || user.includes('<') || user.includes('>') || 
-            user.includes('&')) {
+            user.includes('&') || user.startsWith('.') || user.endsWith('.')) {
                 console.log('A felhasználónév nem tartalmazhat speciális karaktert!');
             }
         }else if ( user.length > 30 || user.length < 6){
-            console.log('Rossz a felhazsnálónév')
+            console.log('Rossz a felhazsnálónév hosszúság')
         }
+        // Password
+        
     })
 
 })
