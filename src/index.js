f0683eff05da6ad2ea0d7059179f70d941e2b607
Zsombor Makker <makker.zsombor@gmail.com>
2022-10-12 14:11:09 +0200

    Username working

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 "use strict";
 document.addEventListener('DOMContentLoaded', () => {
-    var _a;
     // SignUp button
-    (_a = document.getElementById('regisztracio')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
-        //let user = document.getElementById('felhasznalo').
+    document.getElementById('regisztracio').addEventListener('click', () => {
+        let user = document.getElementById('felhasznalo').value;
+        let email = document.getElementById('bemail').value;
+        let pass = document.getElementById('jelszo').value;
+        if (user.length <= 30 && user.length >= 6) {
+            if (user.includes('..') || user.includes(',') || user.includes('+') ||
+                user.includes('-') || user.includes('_') || user.includes('<') || user.includes('>') ||
+                user.includes('&')) {
+                console.log('A felhasználónév nem tartalmazhat speciális karaktert!');
+            }
+        }
+        else if (user.length > 30 || user.length < 6) {
+            console.log('Rossz a felhazsnálónév');
+        }
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,19 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     // SignUp button
-    document.getElementById('regisztracio')?.addEventListener('click', () => {
-        //let user = document.getElementById('felhasznalo').
+    document.getElementById('regisztracio')!.addEventListener('click', () => {
+        let user  = (document.getElementById('felhasznalo') as HTMLInputElement).value;
+        let email = (document.getElementById('bemail') as HTMLInputElement).value;
+        let pass = (document.getElementById('jelszo') as HTMLInputElement).value;
+        if (user.length <= 30 && user.length >= 6) {
+            if (user.includes('..') || user.includes(',') || user.includes('+') || 
+            user.includes('-') || user.includes('_') || user.includes('<') || user.includes('>') || 
+            user.includes('&')) {
+                console.log('A felhasználónév nem tartalmazhat speciális karaktert!');
+            }
+        }else if ( user.length > 30 || user.length < 6){
+            console.log('Rossz a felhazsnálónév')
+        }
     })
 
 })
